f6da114bb34f080846581c426f6a75b65d5990b6
Roman Timoshkin <rmntim@yandex.ru>
2024-11-16 19:17:25 +0300

    refactor: fixup the

--- src/main/java/ru/rmntim/web/utils/AreaChecker.java
+++ src/main/java/ru/rmntim/web/utils/AreaChecker.java
@@ -16,15 +16,15 @@ public class AreaChecker {
     public static boolean isInArea(double x, double y, double r) {
         // Check for the rectangle in the lower-left quadrant
         if (x <= 0 && y <= 0) {
-            return x >= -r && y >= -r/2;
+            return x >= -r && y >= -r;
         }
         // Check for the semicircle in the upper-right quadrant
         else if (x >= 0 && y >= 0) {
-            return (x * x + y * y) <= (r * r) / 4 && x <= r;
+            return (x * x + y * y) <= r * r;
         }
         // Check for the triangle in the lower-right quadrant
         else if (x >= 0 && y <= 0) {
-            return y >= (x - r/2);
+            return y >= (2 * x - r / 2);
         }
         return false;
     }
